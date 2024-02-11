818af89128df2382acb3749541568155d7cbda50
OGBlackDiamond <caden.feller@gmail.com>
2024-02-11 01:16:47 -0800

    changed how the favorites star is negated

--- user-scripts/user.js
+++ user-scripts/user.js
@@ -2,4 +2,5 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.toolbars.bookmarks.visibility", "never");
 user_pref("extensions.activeThemeID", "cheers-bold-colorway@mozilla.org");
 user_pref("extensions.ui.extension.hidden", true);
+user_pref("extensions.pocket.enabled", false);
 user_pref("reader.parse-on-load.enabled", false);
