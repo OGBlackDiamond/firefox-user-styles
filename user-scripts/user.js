efa56ad731d8f9e0fd59e8e4de84ed017c607d2e
OGBlackDiamond <caden.feller@gmail.com>
2024-02-10 23:18:52 -0800

    renamed configure script and removed the reader element from search bar

--- user-scripts/user.js
+++ user-scripts/user.js
@@ -2,3 +2,4 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.toolbars.bookmarks.visibility", "never");
 user_pref("extensions.activeThemeID", "cheers-bold-colorway@mozilla.org");
 user_pref("extensions.ui.extension.hidden", true);
+user_pref("reader.parse-on-load.enabled", false);
