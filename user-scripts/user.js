0599e367b13d2bc06506abf9059455162c0af943
OGBlackDiamond <caden.feller@gmail.com>
2024-02-13 21:45:07 -0800

    forced homepage to be default

--- user-scripts/user.js
+++ user-scripts/user.js
@@ -4,3 +4,6 @@ user_pref("extensions.activeThemeID", "cheers-bold-colorway@mozilla.org");
 user_pref("extensions.ui.extension.hidden", true);
 user_pref("extensions.pocket.enabled", false);
 user_pref("reader.parse-on-load.enabled", false);
+
+// don't let your os choose your default homepage *cough cough* Fedora
+user_pref("browser.startup.homepage", "about:home");
