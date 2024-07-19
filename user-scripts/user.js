bac52cbf0925fbb158a11b4855aa94bc1dc0de43
OGBlackDiamond <caden.feller@gmail.com>
2024-07-19 13:48:35 -0700

    blocked more annoying stuff on startup

--- user-scripts/user.js
+++ user-scripts/user.js
@@ -1,9 +1,21 @@
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.toolbars.bookmarks.visibility", "never");
+
+
 user_pref("extensions.activeThemeID", "cheers-bold-colorway@mozilla.org");
+user_pref("services.sync.prefs.sync-seen.extensions.activeThemeID", true);
+
+user_pref("extensions.ui.dictionary.hidden", true);
+user_pref("extensions.ui.extension.hidden", true);
 user_pref("extensions.ui.extension.hidden", true);
 user_pref("extensions.pocket.enabled", false);
 user_pref("reader.parse-on-load.enabled", false);
 
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.showWeather", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+
 // don't let your os choose your default homepage *cough cough* Fedora
 user_pref("browser.startup.homepage", "about:home");
+
