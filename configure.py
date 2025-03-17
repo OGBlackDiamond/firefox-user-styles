c02d52515eb57d5244835fe0177974299c0f6d56
Caden Feller <caden.feller@gmail.com>
2025-03-17 13:52:52 -0700

    Revert "started rewrite - broken"
    This reverts commit 2dd8c5b6b99db523a5cc3f907dfb82c28bf4c024.

--- configure.py
+++ configure.py
@@ -85,37 +85,49 @@ print()
 # the path for the chrome directory
 profile_chrome_dir = os.path.join(firefox_default_profile, "chrome/") #type: ignore
 
-styles_files = os.listdir(profile_chrome_dir);
+# the path for the userChrome file
+profile_chrome_file = os.path.join(profile_chrome_dir, "userChrome.css")
+
+# the path for the userContent file
+profile_content_file = os.path.join(profile_chrome_dir, "userContent.css")
 
 # the path for the user preferences file
 profile_user_prefs = os.path.join(firefox_default_profile, "user.js") #type: ignore
 
 # values of whether certain directories exist
 chrome_exists = False;
+user_chrome_exists = False;
+user_content_exists = False;
 user_prefs_exists = False;
 
 # checks the existance of all files and directories
 def check_existances():
-    global chrome_exists, user_prefs_exists
+    global chrome_exists, user_chrome_exists, user_content_exists, user_prefs_exists
 
     # checks the existance of certain files
     chrome_exists = os.path.exists(profile_chrome_dir)
+    if chrome_exists:
+        user_chrome_exists = os.path.exists(profile_chrome_file)
+        user_content_exists = os.path.exists(profile_content_file)
     user_prefs_exists = os.path.exists(profile_user_prefs)
 
 # replicates userChrome.css
-def rep_usr_styles():
-    for file in styles_files:
-        file.split('/')[-1]
-        #shutil.copyfile(f"./styles/{file}", file)
-        print(f"Replicating {file}\n")
+def rep_usr_chrome():
+    shutil.copyfile("./styles/userChrome.css", profile_chrome_file)
+    print("Replicating userChrome.css\n")
+
+# replicates userContent.css
+def rep_usr_content():
+    shutil.copyfile("./styles/userContent.css", profile_content_file)
+    print("Replicating userContent.css\n")
 
 # replicates user.js
 def rep_usr_prefs():
-    #shutil.copyfile("./user-scripts/user.js", profile_user_prefs)
+    shutil.copyfile("./user-scripts/user.js", profile_user_prefs)
     print("Enabling legacy toolkit profile customization and cleaning up the toolbar\n")
 
 def mk_chrom_dir():
-    #os.mkdir(profile_chrome_dir)
+    os.mkdir(profile_chrome_dir)
     print("Creating chrome directory\n")
 
 # creates the chrome directory
@@ -131,11 +143,17 @@ def copy_files():
     # ensures the existance of the chrome directory
     check_chrom_dir()
 
-    if not chrome_exists:
-        rep_usr_styles()
+    # checks to make sure that a userChrome file doesn't already exist
+    if not user_chrome_exists:
+        rep_usr_chrome()
     else:
-        print("It looks like you already have a chrome folder!")
-        print("Use \"--delete\" or \"--append\" arguments to make changes.")
+        print("A userChrome.css file already exists! Please remove it with the \"--delete\" argument or append to it with the \"--append\" argument\n")
+
+    # checks to make sure that a userContent file doesn't already exist
+    if not user_content_exists:
+        rep_usr_content()
+    else:
+        print("A userContent.css file already exists! Please remove it with the \"--delete\" argument or append to it with the \"--append\" argument\n")
 
     # checks to make sure we aren't overwriting the user's pre-existing data
     if not user_prefs_exists:
@@ -150,7 +168,8 @@ def copy_files():
 def copy_files_wipe():
     delete(True)
     mk_chrom_dir()
-    rep_usr_styles()
+    rep_usr_chrome()
+    rep_usr_content()
     rep_usr_prefs()
 
 
@@ -161,26 +180,30 @@ def copy_files_append():
     check_chrom_dir()
 
     # appends the userChrome file
-    for file in styles_files:
-        if os.path.exists(file):
-            with open(file, "a") as style:
-                file.split('/')[-1]
-                with open(f"./styles/{file}", "r") as new_style:
-                    #style.write("\n" + new_style.read())
-                    pass
-            print(f"Appended to {file}\n")
-        else:
-            print(f"{file} doesn't exist - creating a fresh one\n")
-            with open(file, "w") as style:
-                #style.write(open(f"./styles/{file}").read())
-                pass
+    if user_chrome_exists:
+        with open(profile_chrome_file, "a") as user_chrome:
+            with open("./styles/userChrome.css", "r") as new_user_chrome:
+                user_chrome.write("\n" + new_user_chrome.read())
+        print("Appended to userChrome.css\n")
+    else:
+        print("userChrome.css doesn't exist - creating a fresh one\n")
+        rep_usr_chrome()
+
+    # appends the userContent file
+    if user_content_exists:
+        with open(profile_content_file, "a") as user_content:
+            with open("./styles/userContent.css", "r") as new_user_content:
+                user_content.write("\n" + new_user_content.read())
+        print("Appended to userContent.css\n")
+    else:
+        print("userContent.css doesnt' exist - creating a fresh one\n")
+        rep_usr_content()
 
     # appends the user preferences file
     if user_prefs_exists:
         with open(profile_user_prefs, "a") as user_prefs:
             with open("./user-scripts/user.js", "r") as new_user_prefs:
-                #user_prefs.write("\n" + new_user_prefs.read())
-                pass
+                user_prefs.write("\n" + new_user_prefs.read())
         print("Appended to user.js\n")
     else:
         print("user.js doesn't exist - creating a fresh one\n")
@@ -191,20 +214,23 @@ def delete(is_for_wipe=False):
     print("Nuclear Launch Detected...")
     # checks if the chrome directory exists
     if chrome_exists:
+        # checks if userChrome.css exists, and deletes it
+        if user_chrome_exists:
+            os.remove(profile_chrome_file)
+            print("Nuking userChrome.css")
 
-        for file in styles_files:
-            #os.remove(file)
-            file.split('/')[-1]
-            print(f"Nuking {file}")
-
+        # checks if userContent.css exists, and deletes it
+        if user_content_exists:
+            os.remove(profile_content_file)
+            print("Nuking userContent.css")
 
         # deletes the chrome directory
-        #os.rmdir(profile_chrome_dir)
+        os.rmdir(profile_chrome_dir)
         print("Nuking chrome directory")
 
     # checks if user.js exists, and deletes it
     if user_prefs_exists:
-        #os.remove(profile_user_prefs)
+        os.remove(profile_user_prefs)
         print("Nuking user.js")
 
     print("\nNuking complete\n")
